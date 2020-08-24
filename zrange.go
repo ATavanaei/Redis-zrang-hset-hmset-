e2f0ac9b6cde6d9c439f32c416c7dedbc664f7d3
a.r.tavanaei <ali_t775@hotmail.com>
2020-08-24 16:51:22 +0430

    Create zrange.go
    it is can set, get value by key, count of data by set min and max in zrange

---
+++ zrange.go
@@ -0,0 +1,58 @@
+package main
+
+import(
+	"github.com/garyburd/redigo/redis"
+	"fmt"
+)
+
+
+func main(){
+
+  client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	c, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		fmt.Println("connect to redis err", err.Error())
+		return
+	}
+
+	/// add zrange data
+	n,err := c.Do("zadd", "curbike", 5, "dsfsdfsdf")
+	if err != nil {
+		panic(err)
+	}else{
+		fmt.Println(n)
+	}
+
+  // get count of datat ion zrange by min and max setting
+	num, err := c.Do("zcard", "curbike")
+	if err != nil {
+		fmt.Println("zcard failed", err.Error())
+	} else {
+		fmt.Printf("curbike's size is %s:", num)
+	}
+
+	// get count of datat in zrange by min and max setting
+	num, err := c.Do("zcount", "curbike", 1, 100)
+	if err != nil {
+		fmt.Println("zcount failed ", err.Error())
+	} else {
+		fmt.Println("zcount num is :", num)
+	}
+
+	/// get value of zrange with key by min and max setting
+  result, err := redis.Values(c.Do("zrange", "curbike", 0, 100))
+	if err != nil {
+		fmt.Println("interstore failed", err.Error())
+	} else {
+		fmt.Printf("interstore newset elsements are:")
+		for _, v := range result {
+			fmt.Printf("%s ", v.([]byte))
+		}
+		fmt.Println()
+	}
+}
